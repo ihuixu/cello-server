fc16bc38863d4238736b6045f32c67e544ac3bfa
xh1021 <xh1021@vip.qq.com>
2015-11-26 20:46:44 +0800

    commonjs

--- base/commonJS.js
+++ base/commonJS.js
@@ -6,17 +6,20 @@ var depends = require('./depends')
 var loader = fs.readFileSync('./lib/loader.js', 'utf8')  
 
 module.exports = function(urlpath, hostPath){
+	var modName = getName(urlpath)
 
-	var config = {
-		path:{
-			"src":"./src/",
-			"dist":"./dist/"
-		}
-	}
+	if(modName == 'loader')
+		return UglifyJS.minify(loader, {fromString: true}).code
 
 	if(fs.existsSync(hostPath)){
-		
 		var configPath = path.join(hostPath, 'config')
+		var config = {
+			path:{
+				"src":"./src/",
+				"dist":"./dist/"
+			}
+		}
+
 		var configs = {}
 
 		if(fs.existsSync(configPath))
@@ -33,16 +36,17 @@ module.exports = function(urlpath, hostPath){
 		}
 
 		var srcPath = path.join(hostPath, config.path.src)
-		var jsfile = depends(srcPath, getName(urlpath)).code
+		var jsfile = depends(srcPath, modName).code
 
 		return UglifyJS.minify(jsfile, {fromString: true}).code
-
-		function getName(urlpath){
-			var reg = new RegExp('^(\/dist\/)|(\.js)$', 'g')
-			return urlpath.replace(reg, '')
-		}
 
 	}
 
 	return ''
 }
+
+function getName(urlpath){
+	var reg = new RegExp('^(\/dist\/)|(\.js)$', 'g')
+	return urlpath.replace(reg, '')
+}
+
