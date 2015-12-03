2b2e5d2922df0bdd24ca83c51f0de1bc363d8c31
xh1021 <xh1021@vip.qq.com>
2015-12-03 18:52:27 +0800

    vue

--- vueJS.js
+++ vueJS.js
@@ -21,7 +21,7 @@ module.exports = function(config, hostPath, mainPath){
 			code.push('var loadStyleLib = require("loadStyle");')
 			code.push('loadStyleLib('+JSON.stringify(source['style'].join(''))+');')
 
-			code.push('var opts = (function(){' + source['script'] + '})();')
+			code.push('var opts = (function(require, exports){' + source['script'] + '})(require, exports);')
 			code.push('opts.template = ' + JSON.stringify('<div class="' + name + '">' + source['template'].join('') + '</div>'))
 			code.push('var components = Vue.extend(opts);')
 			code.push('Vue.component("'+ name +'", components)')
