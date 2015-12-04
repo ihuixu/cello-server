44e97f372211f8a8438fc6eec9b0dc77d569a81c
xh1021 <xh1021@vip.qq.com>
2015-12-04 15:13:26 +0800

    less

--- base/less.js
+++ base/less.js
@@ -1,13 +1,28 @@
+var fs = require("fs")
+var path = require("path")
 var less = require("less")
 var Promise = require("bluebird")
 
+var defaultLess = {
+	'atom': fs.readFileSync('./lib/atom.less', 'utf8')
+}
+
 module.exports = function(block, name, lessPath){
 	return new Promise(function(resolve, reject) {
-		var content = block.scoped 
-				? '.'+ name+'{' + block.content + '}'
-				: block.content
+		var content = []
+		var atomLessPath = path.join(lessPath,'atom.less')
 
-		less.render(content, {
+		if(fs.existsSync(atomLessPath)){
+			content.push(fs.readFileSync(atomLessPath, 'utf8'))
+		}else{
+			content.push(defaultLess['atom'])
+		}
+
+		content.push(block.scoped 
+				? '.'+ name+'{' + block.content + '}'
+				: block.content)
+		
+		less.render(content.join('\n'), {
 			paths:lessPath ? [lessPath] : []
 			, compress:true
 
