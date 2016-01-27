1a6cf3cf5dbfc92e81b7cef8c244ae6974956538
xh1021 <xh1021@vip.qq.com>
2016-01-28 01:48:36 +0800

    defs

---
+++ base/defaults.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var path = require('path')
+
+exports.defaultJS = {
+	'loader' : fs.readFileSync(path.join(__dirname, '../lib/loader.js'), 'utf8')
+	, 'vue' : fs.readFileSync(path.join(__dirname, '../lib/vue.js'), 'utf8')
+}
+exports.defaultCSS = {
+	'cssresetwww': fs.readFileSync(path.join(__dirname, '../lib/cssresetwww.less'), 'utf8')
+	,'cssresetm': fs.readFileSync(path.join(__dirname, '../lib/cssresetm.less'), 'utf8')
+}
+
--- service/compile.js
+++ service/compile.js
@@ -7,14 +7,9 @@ var commonCSS = require('../base/commonCSS')
 var vueJS = require('../base/vueJS')
 var getConfig = require('../config')
 
-var defaultJS = {
-	'loader' : fs.readFileSync(path.join(__dirname, '../lib/loader.js'), 'utf8')
-	, 'vue' : fs.readFileSync(path.join(__dirname, '../lib/vue.js'), 'utf8')
-}
-var defaultCSS = {
-	'cssresetwww': fs.readFileSync(path.join(__dirname, '../lib/cssresetwww.less'), 'utf8')
-	,'cssresetm': fs.readFileSync(path.join(__dirname, '../lib/cssresetm.less'), 'utf8')
-}
+var defaults = require('../base/defaults')
+var defaultJS = defaults.defaultJS
+var defaultCSS = defaults.defaultCSS
 
 function getName(urlpath){
 	var reg = new RegExp('^(\/(dist|css)\/)|(\.(js|css))$', 'g')
