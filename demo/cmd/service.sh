3438c22653c84f390cd6dd50491c1cdabadc72b8
xh1021 <xh1021@vip.qq.com>
2016-01-30 12:08:06 +0800

    22

---
+++ demo/cmd/service.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+rf=$(pwd)'/'
+
+script_path=`dirname $(pwd)`
+script_path=${script_path##*/}
+
+stopService() {
+	echo 'stop server'
+	if [ ! -f $rf'pids' ]; then
+		for proc in `ps -ef | grep node | grep server.js | awk '{print $2}'`; do
+			kill   $proc ; done
+	else
+		cat $rf'pids' | while read line; do
+			#echo 'kill '$line ;
+			kill $line
+		done
+		rm -r $rf'pids'
+	fi 
+
+	}
+startService() {
+	logf=$rf'../log/'` date +%Y/%m/` 
+	log=$logf`date +%d`'.log'
+	echo 'SERVICE START AT '` date +%Y/%m/%d-%T` >> $log
+	mkdir -p $logf
+	echo 'static service start , logfile:'$log	
+	cd $rf'../' && nohup node server.js >> $log &
+
+}
+if [ $# -eq 0 ];then
+	echo "you should pass args start|restart|stop"	
+else
+	case $1 in
+		"stop") 
+			stopService
+			;;
+		"start") 
+			startService
+			;;
+		"restart") 
+			stopService
+			startService
+			;;
+	esac
+fi	
+
---
+++ demo/server.js
@@ -0,0 +1,12 @@
+var server = require('cello-server')
+var config = require('./config.json')
+var fs = require('fs')
+
+server.start(config)
+
+fs.createWriteStream("cmd/pids", {
+flags: "a",
+encoding: "utf-8",
+mode: 0666
+}).write(process.pid + "\n");
+
