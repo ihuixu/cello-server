38c0f86e2277503544a0df7a53b9b2449ed417f8
xh1021 <xh1021@vip.qq.com>
2016-06-27 16:18:26 +0800

    165

--- base/commonCSS.js
+++ base/commonCSS.js
@@ -1,7 +1,18 @@
 var getCSS = require('./getCSS')
+var path = require('path')
 
 module.exports = function(config, mainPath){
-	var mainSource = '@import "'+ mainPath +'.less";'
+	var mainPathArray = mainPath.split(path.sep)
+	var mainSource = ''
+	var modPath = mainPath 
+
+	if(config.corePath && mainPathArray[0] == 'core'){
+		mainPathArray.splice(0,1)
+		modPath = mainPathArray.join(path.sep)
+	}
+
+
+	var mainSource = '@import "'+ modPath +'.less";'
 
 	return getCSS(config, mainSource)
 } 
